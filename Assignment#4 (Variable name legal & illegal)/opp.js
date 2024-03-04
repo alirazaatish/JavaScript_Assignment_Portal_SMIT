aa83c7e7dd6139dc236f5146a4db4f81c0acf453
ALI RAZA <155489997+alirazaatish@users.noreply.github.com>
2024-03-04 06:37:22 +0500

    Add files via upload

---
+++ Assignment#4 (Variable name legal & illegal)/opp.js
@@ -0,0 +1,32 @@
+        //Variable Name Legal & Illegal
+        //Assingment#4
+        //JavaScript
+
+
+        // Question#1
+
+        // Declaring 3 variables in one statement
+        let var1, var2, var3;
+
+        // Legal variable names
+        let myVariable;
+        let _name;
+        let $age;
+        let camelCaseVariable;
+        let snake_case_variable;
+
+        // Question#2
+
+        // Illegal variable names
+        // let 1stVariable; // starts with a number
+        // let my Variable; // contains space
+        // let my-variable; // contains hyphen
+        // let var; // reserved keyword
+        // let break; // reserved keyword
+
+        // Question#3
+        
+        // a) Variable names can only contain letters, numbers, underscores, and dollar signs. For example $my_1stVariable<br>
+        // b) Variables must begin with a letter, underscore, or dollar sign. For example $name, _name, or name<br>
+        // c) Variable names are case-sensitive<br>
+        // d) Variable names should not be JS keywords
