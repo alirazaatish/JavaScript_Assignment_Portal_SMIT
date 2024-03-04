6c466b67545d1338b08b0d62d706c6c18518dd8f
ALI RAZA <155489997+alirazaatish@users.noreply.github.com>
2024-03-04 06:35:54 +0500

    Add files via upload

---
+++ Assignment#1 (Alert)/opp.js
@@ -0,0 +1,21 @@
+            // Alert
+            // Assingment#1
+            // javascript
+
+            // Question # 1
+            function greetVisitor() {
+                alert("Hello! Welcome to our website!");
+            }
+
+            // Question # 2
+            alert("Error! Please enter a valid password.")
+
+            // Question # 3
+            alert("Wellcome to JS Land...\nHappy Coding")
+
+            // Question # 4
+            alert("Wellcome to JS Land...")
+            alert("Happy Coding!\nPrevent this page from creating additional dialogs.")
+
+            // Question # 5
+            console.log("Hello...I can run Js through my web browser's console")
