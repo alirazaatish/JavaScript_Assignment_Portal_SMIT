2c536ceca86a3f6bfbbee79e569a03d91caebeed
ALI RAZA <155489997+alirazaatish@users.noreply.github.com>
2024-03-04 06:36:54 +0500

    Add files via upload

---
+++ Assignment#3 (variable for number)/opp.js
@@ -0,0 +1,27 @@
+         //Variable For Number
+        //Assingment # 3
+        //Javascript
+
+        //Question#1
+        let age="18";
+        alert("I am " + age + " years old.");
+
+        //Question#2
+        var visiter=14;
+        alert("You have visited this site" + visiter + " times");
+
+        //Question#3
+
+        var birthYear="2006";
+        document.write("My birth year is " + birthYear);
+
+        //Question#4
+
+        var visitorInfo={
+            visitorName:"Arman Ahmad",
+            productTitle:"T-Shirt",
+            productQuantity:'4'
+
+        };
+
+        document.write(visitorName + " ordered " + productQuantity + productTitle + " on XYZ Clothing store.");
