52074cdb85f3dcb55a2f1eac75339d43ca677c40
ALI RAZA <155489997+alirazaatish@users.noreply.github.com>
2024-03-04 06:36:18 +0500

    Add files via upload

---
+++ Assignment#2 (Variable for string)/opp.js
@@ -0,0 +1,49 @@
+        // Variable For String
+        // Assingment#2
+        // javascript
+
+        // Question # 1
+        var username;
+
+        // Question # 2
+        var myName="Ali Raza Atish";
+
+        // Question # 3
+        // ....(a)
+        var message;
+        // ....(b)
+        message="Hello World";
+        // ....(c)
+        alert(message)
+
+        // Question # 4
+        var studentBio={
+            fullName:"Ali Raza Atish",
+            age:"15 years old",
+            course:"Certified Mobile Application Development"
+        }
+        alert(studentBio['fullName'])
+        alert(studentBio['age'])
+        alert(studentBio['course'])
+
+        // Question # 5
+        alert("PIZZA\nPIZZ\nPIZ\nPI\nP")
+
+        
+        // Question # 6
+        var email="atishaliraza@gmail.com";
+        alert("My email address is "+email)
+
+        
+        // Question # 7
+        var book="A smarter\nway to learn JavaScript.";
+        alert("I am trying to learn from the Book "+book)
+
+        
+        // Question # 8
+        document.write("Yah! I can write HTML content through JavaScript")
+
+        
+        // Question # 9
+        var symbol="“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”";
+        alert(symbol)
